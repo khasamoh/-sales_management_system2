3e312a8552fc88fa0e328c044d696c33ea0f8b84
khasamoh <khasamoh.12@gmail.com>
2023-06-16 01:47:14 +0300

    implement delete functionality for product

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -14,6 +14,20 @@ const ProductList = () => {
         console.error(error);
       });
   }, []);
+  const handleDelete = async (pro_id) => {
+    const confirmDelete = window.confirm('Are you sure you want to delete Product?');
+    if (confirmDelete) {
+    try {
+      await axios.delete(`http://127.0.0.1:8000/products/delete/${pro_id}/`);
+      console.log('Product deleted successfully');
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+      // Handle deletion error
+    }
+  }
+  };
+
   return (
     <CDBContainer>
       <CDBTable striped>
@@ -36,7 +50,7 @@ const ProductList = () => {
               <td>{product.sale_price}</td>
               <td>
                 <button onClick={/*e => </td>handleSubmit(product.pro_id)*/ product.pro_id} className='btn btn-sm btn-success ms-1 btn-succes'>Edit</button>
-                <button onClick={/*e => </td>handleSubmit(product.pro_id)*/ product.pro_id} className='btn btn-sm btn-danger ms-1 btn-succes'>Delete</button>
+                <button onClick={() => handleDelete(product.pro_id)} className='btn btn-sm btn-danger ms-1 btn-succes'>Delete</button>
               </td>
             </tr>
           ))
